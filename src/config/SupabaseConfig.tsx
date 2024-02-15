b36cae95baebd195d4748a7eb6df483925805f8f
johngaynor <86327580+johngaynor@users.noreply.github.com>
2024-02-14 20:21:48 -0500

    let's try this!

--- src/config/SupabaseConfig.tsx
+++ src/config/SupabaseConfig.tsx
@@ -1,8 +1,8 @@
 import { createClient } from "@supabase/supabase-js";
 
 const supabase = createClient(
-  process.env.SUPABASE_REFERENCE_ID as string,
-  process.env.SUPABASE_PRIVATE_KEY as string
+  process.env.SUPABASE_URL
+  process.env.SUPABASE_SERVICE_ROLE_KEY
 );
 
 export default supabase;
